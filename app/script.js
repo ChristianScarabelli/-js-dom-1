f5daa8419ddaac7039640de3195353ba1b38f81a
ChristianScarabelli <scachmabol@hotmail.it>
2024-10-22 15:49:01 +0200

    ADD: lamp on

--- app/script.js
+++ app/script.js
@@ -1,1 +1,20 @@
 console.log('Primi passi col DOM')
+
+// Richiamo il bottone nello script e lo associo ad una variabile
+const powerLampButton = document.querySelector('.lamp-button')
+console.log(powerLampButton)
+
+// Richiamo l'immagine nello script e la associo ad una variabile
+const lampImg = document.getElementById('lamp-pics')
+console.log(lampImg)
+
+// Aggiungo degli eventi al click del bottone
+powerLampButton.addEventListener('click', function () { 
+    console.log('ho cliccato sul bottone')
+    lampImg.src = './img/yellow_lamp.png'   // Richiamo la variabile della foto negli eventi e cambio la src
+    powerLampButton.innerText = 'Spegni'  // Cambio il testo all'interno dell'elemento html al click del suddetto bottone
+})
+
+
+
+
