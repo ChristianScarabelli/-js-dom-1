1789c48c7161e01fdcdf1eeee7f3d09bb3d52dab
ChristianScarabelli <scachmabol@hotmail.it>
2024-10-22 19:30:02 +0200

    ADD: class for if conditions

--- app/script.js
+++ app/script.js
@@ -11,12 +11,17 @@ console.log(lampImg)
 // Aggiungo degli eventi al click del bottone
 powerLampButton.addEventListener('click', function () { 
     console.log('ho cliccato sul bottone')
-    
-    if (lampImg.src.includes('white_lamp.png')) {  // Richiamo la variabile della foto, se include il nome della foto originaria,
-            lampImg.src = '../img/yellow_lamp.png' // Cambio con la seconda immagine, luce accesa
+ 
+// Verifico se l'immagine ha la classe 'off' (spenta) poi di conseguenza
+    if (lampImg.classList.contains('off')) {
+            lampImg.classList.remove('off') // Rimuovo la classe off
+            lampImg.classList.add('on')  // Aggiungo la classe on
+            lampImg.src = './img/yellow_lamp.png'  // Cambio con la seconda immagine, luce accesa
             powerLampButton.innerText = 'Spegni'   // Cambio il testo all'interno dell'elemento html al click del suddetto bottone
         } else {
-            lampImg.src = '../img/white_lamp.png'  // Torno alla prima immagine, luce spenta
+            lampImg.classList.remove('on')  // ALTRIMENTI sostituisco classe on con classe off
+            lampImg.classList.add('off')  
+            lampImg.src = './img/white_lamp.png'   // Torno alla prima immagine, luce spenta
             powerLampButton.innerText = 'Accendi'  // Cambio il testo all'interno dell'elemento html al click del suddetto bottone
         }
 })
