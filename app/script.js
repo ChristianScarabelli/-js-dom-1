3298788063b2f9f7957c4d4d320d3b91743df823
ChristianScarabelli <scachmabol@hotmail.it>
2024-10-23 10:34:08 +0200

    ADD: corrections

--- app/script.js
+++ app/script.js
@@ -14,16 +14,20 @@ powerLampButton.addEventListener('click', function () {
  
 // Verifico se l'immagine ha la classe 'off' (spenta) poi di conseguenza
     if (lampImg.classList.contains('off')) {
-            lampImg.classList.remove('off') // Rimuovo la classe off
-            lampImg.classList.add('on')  // Aggiungo la classe on
+            // lampImg.classList.remove('off') // Rimuovo la classe off
+            // lampImg.classList.toggle('off')  // Se c'è la classe off, la rimuove
             lampImg.src = './img/yellow_lamp.png'  // Cambio con la seconda immagine, luce accesa
+            lampImg.alt = 'lamp-is-on'  //  Cambio l'etichetta dell'immagine
             powerLampButton.innerText = 'Spegni'   // Cambio il testo all'interno dell'elemento html al click del suddetto bottone
         } else {
-            lampImg.classList.remove('on')  // ALTRIMENTI sostituisco classe on con classe off
-            lampImg.classList.add('off')  
+            // lampImg.classList.add('off')  // Riaggiungo la classe off
+            // lampImg.classList.toggle('off')  // Se non c'è la classe off, la aggiunge
             lampImg.src = './img/white_lamp.png'   // Torno alla prima immagine, luce spenta
+            lampImg.alt = 'lamp-is-off'   //  Cambio l'etichetta dell'immagine
             powerLampButton.innerText = 'Accendi'  // Cambio il testo all'interno dell'elemento html al click del suddetto bottone
         }
+    
+        lampImg.classList.toggle('off')  // E' un'azione fatta a prescindere dalla condizione, quindi può stare fuori dall'if
 })
 
 
